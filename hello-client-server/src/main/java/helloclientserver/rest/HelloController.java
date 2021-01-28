134039f01ec60bb742227289b792a0c3f2209023
Georgi Trendafilov <georgi.trendafilov@dataart.com>
2021-01-28 11:05:57 +0200

    Show port number in order to test Zuul load balancing

--- hello-client-server/src/main/java/helloclientserver/rest/HelloController.java
+++ hello-client-server/src/main/java/helloclientserver/rest/HelloController.java
@@ -1,6 +1,7 @@
 package helloclientserver.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,15 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/rest/hello/server")
 public class HelloController {
 
+    @Value("${server.port}")
+    private int serverPort;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping
     @ResponseBody
     public String hello() {
-        return "Hello from the Kur server side!";
+        return "Hello from the Kur server side running on port: " + serverPort;
     }
 }
